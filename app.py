85f83adf1822c01a079e16c9b5af113ca9ae2f2e
yashvendravyas <yashvendravyas@gmail.com>
2026-09-06 18:23:23 +0530

    Update app.py
    nltk error reset

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pickle
 import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import string 
